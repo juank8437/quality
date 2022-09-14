c6201baff42bf734a4080a505fe819cdf3c81ebf
juank8437 <juank8437@hotmail.com>
2022-09-14 19:12:52 -0400

    Varios modelos tomados de Codecademy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CollapsibleExample from './Navbar.js';
+import {Input} from './Input.js';
 
 function App() {
   return (
@@ -18,6 +23,92 @@ function App() {
           Aprendamos React, ahora que ya pudimos desplegar el sitio en Netlify
         </a>
       </header>
+      <br></br>
+      <CollapsibleExample />
+      <br></br>
+      <Input />
+      <br></br>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <br></br>
+      <div class="row">
+        <div class="col-sm-3">
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+        </div>
+        <div class="col-sm-3">
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+        </div>
+        <div class="col-sm-3">
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+        </div>
+        <div class="col-sm-3">
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-sm-3">
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+        </div>
+      </div>
+
     </div>
   );
 }
